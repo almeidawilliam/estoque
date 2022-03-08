d7bda2aba83eb92c0381953a999a53e05ec23379
William Almeida <almeida.ws@hotmail.com>
2022-03-07 21:25:51 -0300

    class 3.6 - Refatorando o código

--- app/src/main/java/br/com/alura/estoque/ui/activity/ListaProdutosActivity.java
+++ app/src/main/java/br/com/alura/estoque/ui/activity/ListaProdutosActivity.java
@@ -2,7 +2,6 @@ package br.com.alura.estoque.ui.activity;
 
 import android.os.AsyncTask;
 import android.os.Bundle;
-import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.RecyclerView;
@@ -47,39 +46,36 @@ public class ListaProdutosActivity extends AppCompatActivity {
     }
 
     private void buscaProdutos() {
+        buscaProdutosInternos();
+    }
 
-        ProdutoService produtoService = new EstoqueRetrofit().getProdutoService();
-        Call<List<Produto>> call = produtoService.buscaTodos();
-
+    private void buscaProdutosInternos() {
         new BaseAsyncTask<>(
                 dao::buscaTodos,
                 resultado -> {
                     adapter.atualiza(resultado);
 
-                    new BaseAsyncTask<>(
-                            () -> {
-                                try {
-                                    Thread.sleep(3000);
-                                    Response<List<Produto>> resposta = call.execute();
-                                    List<Produto> produtosNovos = resposta.body();
-                                    dao.salva(produtosNovos);
-                                } catch (IOException | InterruptedException e) {
-                                    e.printStackTrace();
-                                }
-                                return dao.buscaTodos();
-                            },
-                            produtosNovos -> {
-                                if (produtosNovos != null) {
-                                    adapter.atualiza(produtosNovos);
-                                } else {
-                                    Toast.makeText(
-                                            this,
-                                            "não foi possível buscar os produtos da API",
-                                            Toast.LENGTH_SHORT
-                                    ).show();
-                                }
-                            }).executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR);
+                    buscaProdutosNaApi();
                 }).execute();
+    }
+
+    private void buscaProdutosNaApi() {
+        ProdutoService produtoService = new EstoqueRetrofit().getProdutoService();
+        Call<List<Produto>> call = produtoService.buscaTodos();
+        new BaseAsyncTask<>(
+                () -> {
+                    try {
+                        Response<List<Produto>> resposta = call.execute();
+                        List<Produto> produtosNovos = resposta.body();
+                        dao.salva(produtosNovos);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                    return dao.buscaTodos();
+                },
+                produtosNovos ->
+                        adapter.atualiza(produtosNovos)
+        ).executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR);
     }
 
     private void configuraListaProdutos() {
